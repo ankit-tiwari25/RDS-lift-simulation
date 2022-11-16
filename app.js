363de0540aa6a0f3db195d1e7a0c2a95cc839e7a
Ankit Tiwari <ankit.tiwari25@outlook.com>
2022-11-16 13:29:56 +0530

    layout-improved significantly using flex

--- app.js
+++ app.js
@@ -1,100 +1,151 @@
 
- let submitButton = document.getElementById("submit");
+// Getting submit button
+const submitButton = document.getElementById("submit");
+let initialFloorCount;
+submitButton.addEventListener("click", function(){
+    //hide form elements and show lift simulation div
+    hideFormElements();
+    let floorCount = document.getElementById("floor-count").value;
+    let liftCount = document.getElementById("lift-count").value;
 
- submitButton.addEventListener("click", function() {
+    initialFloorCount = floorCount;
 
-    // Hide Input Fields
-    hideInputFields();
+    console.log("Submit button clicked");
+    console.log("floor count : " + floorCount);
+    console.log("lift count : " + liftCount);
+    addFloorContainer(floorCount, liftCount);
 
-    // Display output
+});
 
-    let floorCount = document.getElementById('floors').value;
-    let liftCount = document.getElementById('lifts').value;
-
-    displayOutput(floorCount, liftCount);
-    
-    console.log("submit clicked");
-    console.log(floorCount);
-    console.log(liftCount);
- });
-
- function hideInputFields(){
-    let formContainer = document.getElementById("container-form");
+function hideFormElements(){
+    const formContainer = document.getElementById("container-form");
     formContainer.style.display = "none";
+    const lsContainer = document.getElementById('ls-container');
+    lsContainer.style.display = "block";
  }
 
- function displayOutput(floorNumber,liftNumber){
+ function addFloorContainer(floorCount,liftCount){
 
-    // display top-floor
-    let initialFloorCount = floorNumber;
-   
-
-   
-    while(floorNumber > 0){
+    while(floorCount > 0){
+        //create floor container div
+        const floorContainer = createFloorContainer(floorCount, liftCount);
+        floorCount--;
+        addInLSContainer(floorContainer);
+            
+        
+    }
     
-    const floorContainer = document.createElement('div');
-    floorContainer.classList.add('container-floor');
+ }
 
+ function createFloorContainer(floorCount,liftCount){
+
+    //create floor container div
+    let floorContainer = document.createElement('div');
+    floorContainer.classList.add("floor-container");
+
+    // create button container
+    const buttonContainer = createButtonContainer(floorCount, liftCount);
+    const  elevatorContainer = createElevatorContainer(floorCount, liftCount);
+    
+    
+    const textContainer = createTextContainer(floorCount);
+    
+
+    
+    floorContainer.append(buttonContainer);
+    floorContainer.append(elevatorContainer)
+   //  floorContainer.append(hrContainer);
+    floorContainer.append(textContainer);
+
+    return floorContainer;
+    
+    
+ }
+
+ function addInLSContainer(floorContainer){
+    // adding to main div
+    const lsContainer = document.getElementById('ls-container');
+    lsContainer.append(floorContainer);
+ }
+
+
+ function createButtonContainer(floorCount, liftCount){
     const buttonContainer = document.createElement('div');
     buttonContainer.classList.add('button-container');
-    
-    if(floorNumber != initialFloorCount){
-    const buttonUp = document.createElement('button');
+
+    // Button up
+   if(floorCount != initialFloorCount){
+      const buttonUp = document.createElement('button');
     buttonUp.classList.add('button-up');
     buttonUp.innerText = "Up";
     buttonContainer.append(buttonUp);
-      }
+   }
 
-      if(floorNumber != 1){
-    const buttonDown = document.createElement('button');
-    buttonDown.classList.add('button-down');
-    buttonDown.innerText = "Down";
-    buttonContainer.append(buttonDown);
-      }
-
-    
-    
-
-    floorContainer.append(buttonContainer); 
-
-    if(floorNumber == 1){
-        // Add lifts to ground floor...
-
-        const  elevatorContainer = document.createElement('div');
-        elevatorContainer.classList.add('elevator-container');
-
-
-            for(let i = 1; i <= liftNumber; i++){
-                let elevator = document.createElement('div');
-            elevator.classList.add('elevator-2');
-
-            if( i == 1){
-                elevator.style.marginLeft = "100px";
-            }
-                elevatorContainer.append(elevator);
-            }
-        floorContainer.append(elevatorContainer)
+    //Button Down
+    if(floorCount != 1){
+      const buttonDown = document.createElement('button');
+      buttonDown.classList.add('button-down');
+      buttonDown.innerText = "Down";
+      buttonContainer.append(buttonDown);
     }
-   
 
-    const floor = document.createElement('hr');
-    const floorText = document.createElement('h3');
+    return buttonContainer;
+ }
 
-    floorText.innerText = "Floor " + floorNumber;
-    floorContainer.append(floor);
-    floorContainer.append(floorText);
+ function createElevatorContainer(floorCount, liftCount){
+    const elevatorContainer = document.createElement('div');
+    elevatorContainer.classList.add('elevator-container');
 
-   
+      // elevator container contains lift container + hr container
 
-    // Root element
-
-    let mainContainer = document.querySelector('main');
-
-    mainContainer.append(floorContainer);
-
-    // console.log(mainContainer);
+      
+         //call lift container container only lifts
+      const liftContainer = createLiftContainer(floorCount, liftCount);
     
-    floorNumber--;
-}
+
+      // hr container - always
+      const hrContainer = createHRContainer();
+
+    elevatorContainer.append(liftContainer);
+    elevatorContainer.append(hrContainer);
+   return elevatorContainer;
 
  }
+
+ function createHRContainer(){
+    const hrContainer = document.createElement('div');
+    hrContainer.classList.add('hr-container');
+
+    const hr = document.createElement('hr');
+    hrContainer.append(hr);
+
+    return hrContainer;
+ }
+
+ function createTextContainer(floorNumber){
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('text-container');
+
+    const textElement = document.createElement('h3');
+    textElement.innerText = "Floor " + floorNumber;
+
+    textContainer.append(textElement);
+    return textContainer;
+ }
+
+ function createLiftContainer(floorCount,liftCount){
+
+    // Add elevators in elevator container based on lift count
+    const liftContainer = document.createElement('div');
+    liftContainer.classList.add('lift-container')
+    while(liftCount > 0 && floorCount == 1){
+      const singleLift = document.createElement('div');
+      singleLift.classList.add('lift');
+      // singleLift.style.marginLeft = 50 * floorCount +" px";
+      liftContainer.append(singleLift);
+      liftCount--;
+  }
+  return liftContainer;
+ }
+
+ 
